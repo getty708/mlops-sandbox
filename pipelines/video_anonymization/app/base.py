84e7264fe84ef6dd4e31a7a2edb81032938ae4f8
Naoya Yoshimura <yoshimura708x@gmail.com>
2024-09-21 06:44:27 +0000

    feat: refactor

---
+++ pipelines/video_anonymization/app/base.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+import cv2
+import numpy as np
+from loguru import logger
+from opentelemetry import trace
+
+MAX_FRAME_ID_IN_DEBUG_MODE = 10
+
+DEFAULT_OUTPUT_DIR = Path("./outputs/")
+FULLHD_IMAGE_SIZE_HW = (1080, 1920)
+
+
+class BasePipeline:
+    def __init__(
+        self,
+        video_path: Path,
+        proc_tracer: trace.Tracer,
+        output_dir: Path = DEFAULT_OUTPUT_DIR,
+        frame_size_hw: tuple[int, int] = FULLHD_IMAGE_SIZE_HW,
+        debug: bool = False,
+    ):
+        self.video_path = video_path
+        self.output_dir = output_dir
+        self.frame_size_hw = frame_size_hw
+        self.debug = debug
+
+        # Monitoring
+        self.proc_tracer = proc_tracer
+
+        # Init videos
+        self.cap = cv2.VideoCapture(str(video_path))
+        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.frame_rate = int(self.cap.get(cv2.CAP_PROP_FPS))
+        logger.info(
+            f"Start pipeline with video: {video_path} "
+            f"(frame count = {self.frame_count}, frame rate = {self.frame_rate})"
+        )
+
+        # Outputs
+        self.output_dir.mkdir(parents=True, exist_ok=True)
+        self.output_frame_dir = output_dir / "frames"
+        self.output_frame_dir.mkdir(parents=True, exist_ok=True)
+
+        self.output_video_path = output_dir / f"{video_path.stem}_anonymized.mp4"
+        logger.info(f"Output video path: {self.output_video_path}")
+
+    def init_pipeline_modules(self):
+        raise NotImplementedError
+
+    def run(self, draw_model_outputs: bool = False):
+        # Init video writer
+        fourcc = cv2.VideoWriter_fourcc(*"avc1")
+        video_writer = cv2.VideoWriter(
+            str(self.output_video_path),
+            fourcc,
+            self.frame_rate,
+            (self.frame_size_hw[1], self.frame_size_hw[0]),
+        )
+
+        # Process frames
+        frame_id = 0
+        while True:
+            frame_id += 1
+            try:
+                with self.proc_tracer.start_as_current_span("process_single_frame"):
+                    # Get next frame
+                    is_success, frame = self.cap.read()
+                    if not is_success:
+                        break
+                    logger.info(f"Process frame {frame_id}/{self.frame_count}")
+
+                    # Run inferences
+                    frame = self.process_single_frame(frame)
+
+                    # Save Images
+                    video_writer.write(frame)
+                    if self.debug:
+                        img_path = self.output_frame_dir / f"img_{frame_id}.jpg"
+                        cv2.imwrite(str(img_path), frame)
+
+                    if self.debug and (frame_id >= MAX_FRAME_ID_IN_DEBUG_MODE):
+                        logger.info(
+                            f"Reached max frame id in debug mode: {MAX_FRAME_ID_IN_DEBUG_MODE}"
+                        )
+                        break
+            except KeyboardInterrupt:
+                break
+
+        logger.info("Stop pipeline ...")
+        video_writer.release()
+
+    def process_single_frame(self, img: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
--- pipelines/video_anonymization/app/run_pipeline.py
+++ pipelines/video_anonymization/app/run_pipeline.py
@@ -13,6 +13,8 @@ from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from pipelines.video_anonymization.app.base import BasePipeline
 from services.detection.detr.model import DetrWrapper
 from services.detection.visualize import dwraw_bounding_boxes
 from services.pose_estimation.transpose import TransPoseWrapper
@@ -28,6 +30,7 @@ _MSCOCO_NOSE_JOINT_INDEX = 0
 _MSCOCO_LEFT_SHOULDER_JOINT_INDEX = 5
 _MSCOCO_RIGHT_SHOULDER_JOINT_INDEX = 6
 
+OTEL_CONNECTOR_ENDPOINT = "otel-collector:4317"
 
 # ====================
 # OpenTelemetry Setup
@@ -40,14 +43,14 @@ trace.set_tracer_provider(tracer_provider=tracer_provider)
 # Send traces to OtelCollector.
 tracer_provider.add_span_processor(
     span_processor=BatchSpanProcessor(
-        span_exporter=OTLPSpanExporter(endpoint="otel-collector:4317", insecure=True)
+        span_exporter=OTLPSpanExporter(endpoint=OTEL_CONNECTOR_ENDPOINT, insecure=True)
     )
 )
 tracer = trace.get_tracer_provider().get_tracer(__name__)
 
 # == Metrics ==
 reader = PeriodicExportingMetricReader(
-    OTLPMetricExporter(endpoint="otel-collector:4317", insecure=True),
+    OTLPMetricExporter(endpoint=OTEL_CONNECTOR_ENDPOINT, insecure=True),
     export_interval_millis=1000,
 )
 metrics.set_meter_provider(MeterProvider(resource=resource, metric_readers=[reader]))
@@ -69,6 +72,8 @@ class PeopeleCounter:
         people_counter.add(difference)
         self.current_peopel_count = new_value
 
+
+pcounter = PeopeleCounter()
 
 # ===============
 #  Drawing Utils
@@ -106,98 +111,33 @@ def draw_face_anonymization_mask(img: np.ndarray, keypoints_all: np.ndarray) -> 
 # ==========
 
 
-class Pipeline:
-    def __init__(
-        self,
-        video_path: Path,
-        output_dir: Path = DEFAULT_OUTPUT_DIR,
-        frame_size_hw: tuple[int, int] = FULLHD_IMAGE_SIZE_HW,
-        debug: bool = False,
-    ):
-        self.video_path = video_path
-        self.output_dir = output_dir
-        self.frame_size_hw = frame_size_hw
-        self.debug = debug
-        self.people_counter = PeopeleCounter()
+class Pipeline(BasePipeline):
+    detector: DetrWrapper
+    pose_estimator: TransPoseWrapper
 
-        # Init videos
-        self.cap = cv2.VideoCapture(str(video_path))
-        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.frame_rate = int(self.cap.get(cv2.CAP_PROP_FPS))
-        logger.info(
-            f"Start pipeline with video: {video_path} "
-            f"(frame count = {self.frame_count}, frame rate = {self.frame_rate})"
-        )
+    def __init__(self, *args, draw_model_outputs: bool = False, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.draw_model_outputs = draw_model_outputs
 
-        # Outputs
-        self.output_dir.mkdir(parents=True, exist_ok=True)
-        self.output_frame_dir = output_dir / "frames"
-        self.output_frame_dir.mkdir(parents=True, exist_ok=True)
-
-        self.output_video_path = output_dir / f"{video_path.stem}_anonymized.mp4"
-        logger.info(f"Output video path: {self.output_video_path}")
-
-        # Init models
+    def init_pipeline_modules(self):
         self.detector = DetrWrapper(device=_DEVICE)
         self.pose_estimator = TransPoseWrapper(device=_DEVICE)
 
-    def run(self, draw_model_outputs: bool = False):
-        # Init video writer
-        fourcc = cv2.VideoWriter_fourcc(*"avc1")
-        video_writer = cv2.VideoWriter(
-            str(self.output_video_path),
-            fourcc,
-            self.frame_rate,
-            (self.frame_size_hw[1], self.frame_size_hw[0]),
-        )
-
-        # Process frames
-        frame_id = 0
-        while True:
-            frame_id += 1
-            try:
-                with tracer.start_as_current_span("process_single_frame"):
-                    # Get next frame
-                    is_success, frame = self.cap.read()
-                    if not is_success:
-                        break
-                    logger.info(f"Process frame {frame_id}/{self.frame_count}")
-
-                    # Run inferences
-                    frame = self.process_single_frame(frame, draw_model_outputs=draw_model_outputs)
-
-                    # Save Images
-                    video_writer.write(frame)
-                    if self.debug:
-                        img_path = self.output_frame_dir / f"img_{frame_id}.jpg"
-                        cv2.imwrite(str(img_path), frame)
-
-                    if self.debug and (frame_id >= MAX_FRAME_ID_IN_DEBUG_MODE):
-                        logger.info(
-                            f"Reached max frame id in debug mode: {MAX_FRAME_ID_IN_DEBUG_MODE}"
-                        )
-                        break
-            except KeyboardInterrupt:
-                break
-
-        logger.info("Stop pipeline ...")
-        video_writer.release()
-
-    def process_single_frame(self, img: np.ndarray, draw_model_outputs: bool = False) -> np.ndarray:
+    def process_single_frame(self, img: np.ndarray) -> np.ndarray:
         img_tensor = torch.from_numpy(img).permute(2, 0, 1).flip(0).unsqueeze(0).to(_DEVICE)
         img_tensor = img_tensor.float() / 255.0
 
         # == Model Inference ==
         with tracer.start_as_current_span("person_detection"):
             bboxes_xyxy = self.detector(img_tensor)
-            self.people_counter.add(len(bboxes_xyxy))
+            pcounter.add(len(bboxes_xyxy))
         with tracer.start_as_current_span("pose_estimation"):
             keypoints = self.pose_estimator(img_tensor, bboxes_xyxy)
 
         # == Visualize Results ==
         with tracer.start_as_current_span("visualization"):
             img = draw_face_anonymization_mask(img, keypoints.numpy())
-            if draw_model_outputs:
+            if self.draw_model_outputs:
                 img = dwraw_bounding_boxes(img, bboxes_xyxy.numpy())
                 img = draw_keypoints(img, keypoints.numpy())
         return img
@@ -223,8 +163,10 @@ class Pipeline:
 def main(video_path: Path, debug: bool = False, draw_model_outputs: bool = False):
     pipeline = Pipeline(
         video_path=video_path,
+        proc_tracer=tracer,
         debug=debug,
     )
+    pipeline.init_pipeline_modules()
     pipeline.run(draw_model_outputs=draw_model_outputs)
 
 
